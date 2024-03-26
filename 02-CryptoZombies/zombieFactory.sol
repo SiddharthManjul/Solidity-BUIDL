fd6c0235826f6b81ea8e4ae501a6856e970abc65
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-03-26 23:44:49 +0530

    safemath.sol updated for SafeMath32 & SafeMath16

--- 02-CryptoZombies/zombieFactory.sol
+++ 02-CryptoZombies/zombieFactory.sol
@@ -7,6 +7,8 @@ import "../03-Tokens/safeMath.sol";
 
 contract ZombieFactory is Ownable {
     using SafeMath for uint256;
+    using SafeMath32 for uint32;
+    using SafeMath16 for uint16;
 
     event NewZombie(uint zombieId, string name, uint dna);
 
@@ -33,7 +35,7 @@ contract ZombieFactory is Ownable {
             Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)
         ) - 1;
         zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
+        ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
         emit NewZombie(id, _name, _dna);
     }
 
