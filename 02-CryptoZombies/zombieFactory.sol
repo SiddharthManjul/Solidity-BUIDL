0ae3c24dba4e53e57e2c9356bbb7abae3649fdd2
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-03-26 22:37:40 +0530

    Zombie Battle System

--- 02-CryptoZombies/zombieFactory.sol
+++ 02-CryptoZombies/zombieFactory.sol
@@ -16,6 +16,8 @@ contract ZombieFactory is Ownable {
         uint dna;
         uint32 level;
         uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
     }
 
     Zombie[] public zombies;
@@ -25,7 +27,7 @@ contract ZombieFactory is Ownable {
 
     function _createZombie(string memory _name, uint _dna) internal {
         uint id = zombies.push(
-            Zombie(_name, _dna, 1, uint32(now + cooldownTime))
+            Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)
         ) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
